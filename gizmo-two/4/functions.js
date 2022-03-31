4ceefe83dc4e56af5c03d8a1ef0b68b308cb523e
Lizzy Shin <98031376+shinsy3@users.noreply.github.com>
2022-03-31 18:30:56 -0400

    1

---
+++ gizmo-two/4/functions.js
@@ -0,0 +1,4 @@
+document.body.onmousemove = function(e) {
+document.documentElement.style.setProperty('--x', (e.clientX+window.scrollX) + 'px');
+document.documentElement.style.setProperty('--y', (e.clientY+window.scrollY) + 'px');
+}
